79dd00701529fd44235dfbe958183b613d5f71b2
Cory <corystephens012@gmail.com>
2021-09-15 20:09:02 -0400

    fully functional back end

--- db/migrate/20210914155734_create_reviews.rb
+++ db/migrate/20210914155734_create_reviews.rb
@@ -1,8 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      
       t.string :comment
       t.integer :rating
+
+
       t.belongs_to :owner
       t.belongs_to :cleaner
       t.belongs_to :pool
